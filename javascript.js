ce5fb19c49f91b69371bfdd74b69d982ed9aa7e2
Emma Slager <emmajean.slager@gmail.com>
2023-02-28 15:25:39 -0800

    File upload

---
+++ javascript.js
@@ -0,0 +1,131 @@
+var map = L.map('map').setView([47.258728, -122.465973], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZWpzbGdyIiwiYSI6ImNrMnBtbDF0YzA1d2UzbnJtbHMxcWltOTIifQ.MY_BZXu-EPtwTlxsI_zgcQ'
+}).addTo(map);
+
+var drawnItems = L.featureGroup().addTo(map);
+
+var tableData = L.layerGroup().addTo(map);
+var url = "https://gisdb.xyz/sql?q=";
+// change the Query below by replacing lab_7_name with your table name
+var sqlQuery = "SELECT * FROM tahoma";
+function addPopup(feature, layer) {
+    layer.bindPopup(
+        "<b>" + feature.properties.name + "</b><br>" +
+        feature.properties.description
+    );
+}
+
+fetch(url + sqlQuery)
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+        L.geoJSON(data, {onEachFeature: addPopup}).addTo(tableData);
+    });
+
+new L.Control.Draw({
+    draw : {
+        polygon : true,
+        polyline : true,
+        rectangle : false,     // Rectangles disabled
+        circle : false,        // Circles disabled
+        circlemarker : false,  // Circle markers disabled
+        marker: true
+    },
+    edit : {
+        featureGroup: drawnItems
+    }
+}).addTo(map);
+
+function createFormPopup() {
+    var popupContent =
+        '<form>' +
+        'Description:<br><input type="text" id="input_desc"><br>' +
+        'User\'s Name:<br><input type="text" id="input_name"><br>' +
+        '<input type="button" value="Submit" id="submit">' +
+        '</form>'
+    drawnItems.bindPopup(popupContent).openPopup();
+}
+
+map.addEventListener("draw:created", function(e) {
+    e.layer.addTo(drawnItems);
+    createFormPopup();
+});
+
+function setData(e) {
+
+    if(e.target && e.target.id == "submit") {
+
+        // Get user name and description
+        let enteredUsername = document.getElementById("input_name").value;
+        let enteredDescription = document.getElementById("input_desc").value;
+
+        // For each drawn layer
+        drawnItems.eachLayer(function(layer) {
+            
+            // Create SQL expression to insert layer
+            let drawing = JSON.stringify(layer.toGeoJSON().geometry);
+            let sql =
+                "INSERT INTO tahoma (geom, name, description) " +
+                "VALUES (ST_SetSRID(ST_GeomFromGeoJSON('" +
+                drawing + "'), 4326), '" +
+                enteredUsername + "', '" +
+                enteredDescription + "')";
+            console.log(sql);
+
+            // Send the data
+            fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: "q=" + encodeURI(sql)
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log("Data saved:", data);
+            })
+            .catch(function(error) {
+                console.log("Problem saving the data:", error);
+        });
+
+        // Transfer submitted drawing to the tableData layer 
+        //so it persists on the map without you having to refresh the page
+        var newData = layer.toGeoJSON();
+        newData.properties.description = enteredDescription;
+        newData.properties.name = enteredUsername;
+        L.geoJSON(newData, {onEachFeature: addPopup}).addTo(tableData);
+
+    });
+
+        // Clear drawn items layer
+        drawnItems.closePopup();
+        drawnItems.clearLayers();
+
+    }
+}
+
+document.addEventListener("click", setData);
+
+map.addEventListener("draw:editstart", function(e) {
+    drawnItems.closePopup();
+});
+map.addEventListener("draw:deletestart", function(e) {
+    drawnItems.closePopup();
+});
+map.addEventListener("draw:editstop", function(e) {
+    drawnItems.openPopup();
+});
+map.addEventListener("draw:deletestop", function(e) {
+    if(drawnItems.getLayers().length > 0) {
+        drawnItems.openPopup();
+    }
+});
